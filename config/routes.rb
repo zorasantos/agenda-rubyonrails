d63ca778ffdb9dacc695649775b470e10e879519
Zora Santos <zoranildo22@gmail.com>
2020-12-24 12:15:48 -0300

    Api concluída

---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+  Rails.application.routes.draw do
+    devise_for :users
+    namespace :api do
+      namespace :v1 do
+        resources :contacts
+      end
+    end
+  
+    # constraints subdomain: 'api' do
+    #   scope module: 'api' do
+    #     namespace :v1 do
+    #       resources :contacts
+    #     end
+    #   end
+    # end
+  end
+
